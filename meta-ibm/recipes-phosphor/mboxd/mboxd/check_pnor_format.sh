0e17e9062252f08ec3e2afc1f3a1c0b575cb6a5d
Adriana Kobylak <anoo@us.ibm.com>
2019-10-25 10:56:17 -0500

    check-pnor-format: Call ubiformat if PNOR chip is blank
    For rainier simics using a single image (no PNOR file), the PNOR
    chip would be modeled as empty. In that case the check-pnor-format
    service fails.
    
    We need the empty PNOR to be formatted as UBI because:
    1. The check-pnor-format service has a RequiredBy mboxd causing mboxd
       fail to start if the check-pnor-format fails.
    2. There are PNOR patches that the simics session automatically
       populates, so the PNOR needs to be in a UBI format for the
       pnor patch service to create the patch volume.
    
    The ubiformat takes less than 1s to run in simics.
    
    Tested: Verified the PNOR was formatted as UBI, mboxd starts, and
            the PNOR patch directory was mounted and populated.
    
    Jan 17 21:38:00 rainier check_pnor_format.sh[227]: /dev/mtd7 is blank, formatting as UBI
    Jan 17 21:38:00 rainier kernel: ubi7: attaching mtd7
    ...
    Jan 17 21:38:05 rainier systemd[1]: Started Create a ubi volume for pnor patches.
    
    Change-Id: I4d901f38003259c48f56f7992d06c988c26b659d
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- meta-ibm/recipes-phosphor/mboxd/mboxd/check_pnor_format.sh
+++ meta-ibm/recipes-phosphor/mboxd/mboxd/check_pnor_format.sh
@@ -13,6 +13,14 @@ pnor="${pnormtd#mtd}"
 pnordev="/dev/mtd${pnor}"
 
 if [[ ! "$(dd if=${pnordev} bs=1 count=3 2> /dev/null)" = "UBI" ]]; then
-  echo "${pnordev} is not formatted UBI"
-  exit 1
+  # Check if the pnor is blank (all 0xFs), if it is then format it as UBI to
+  # support patch files. This is a workaround to use single image on simics.
+  if [[ "$(hexdump -n 4 ${pnordev})" = *"ffff ffff"* ]]; then
+    echo "${pnordev} is blank, formatting as UBI"
+    ubiformat "${pnordev}" -y -q
+    exit 0
+  else
+    echo "${pnordev} is not formatted UBI"
+    exit 1
+  fi
 fi
