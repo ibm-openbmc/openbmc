openbmc/meta-ibm

Rainier: Enable supply-monitor-ng
Rainier will use the "next generation" power supply monitor. Enable
supply-monitor-ng.

Change-Id: I315098c20992baac77d26fe20634d43002cc77b9
Signed-off-by: Brandon Wyman <bjwyman@gmail.com>

recipes-phosphor/power/phosphor-power_%.bbappend
@@ -5,7 +5,7 @@ inherit obmc-phosphor-systemd
 SRC_URI += "file://psu.json"
 
 EXTRA_OEMESON_append_ibm-ac-server = " -Ducd90160-yaml=${STAGING_DIR_HOST}${datadir}/power-sequencer/ucd90160.yaml"
-EXTRA_OEMESON_append_rainier = " -Ducd90160-yaml=${STAGING_DIR_HOST}${datadir}/power-sequencer/ucd90160.yaml -Dibm-vpd=true"
+EXTRA_OEMESON_append_rainier = " -Ducd90160-yaml=${STAGING_DIR_HOST}${datadir}/power-sequencer/ucd90160.yaml -Dibm-vpd=true -Dsupply-monitor-ng=true"
 EXTRA_OEMESON_append_mihawk = " -Dpower_sequencer=mihawk-cpld"
 
 DEPENDS_append_ibm-ac-server = " power-sequencer"

ibm-openbmc/openbmc

meta-ibm/recipes-phosphor/power/phosphor-power_%.bbappend
@@ -5,7 +5,7 @@ inherit obmc-phosphor-systemd
 SRC_URI += "file://psu.json"
 
 EXTRA_OEMESON_append_ibm-ac-server = " -Ducd90160-yaml=${STAGING_DIR_HOST}${datadir}/power-sequencer/ucd90160.yaml"
-EXTRA_OEMESON_append_p10bmc = " -Ducd90160-yaml=${STAGING_DIR_HOST}${datadir}/power-sequencer/ucd90160.yaml -Dibm-vpd=true"
+EXTRA_OEMESON_append_p10bmc = " -Ducd90160-yaml=${STAGING_DIR_HOST}${datadir}/power-sequencer/ucd90160.yaml -Dibm-vpd=true -Dsupply-monitor-ng=true"
 EXTRA_OEMESON_append_mihawk = " -Dpower_sequencer=mihawk-cpld"
 
 DEPENDS_append_ibm-ac-server = " power-sequencer"