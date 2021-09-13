e275d09fe05de301595df22c2864808d0a9e273c
Eddie James <eajames@linux.ibm.com>
2021-09-13 14:54:40 -0500

    ibm: p10bmc: Fix everest mux workaround
    As a result of missing device tree changes, combined with the bash -e
    flag, the gpio keys driver wasn't getting re-bound. Fix this by setting
    a flag for each mux device and only binding it if it existed to begin
    with.
    
    Signed-off-by: Eddie James <eajames@linux.ibm.com>
    Change-Id: I7f53eca13caa594dc5fb7d8c33a2a5e64f42723d

--- meta-ibm/recipes-phosphor/chassis/mux-workarounds/mux-workarounds.sh
+++ meta-ibm/recipes-phosphor/chassis/mux-workarounds/mux-workarounds.sh
@@ -24,6 +24,16 @@ reset_bus_15() {
 
 # everest system only
 if [ $(fw_printenv fitconfig | grep everest) ]; then
+    mux4=
+    mux5=
+    mux6=
+    mux140=
+    mux141=
+    mux150=
+    mux151=
+    mux152=
+    echo "Detected everest system; performing I2C mux workarounds"
+
     gpiopath="/sys/bus/platform/drivers/gpio-keys-polled"
     drvpath="/sys/bus/i2c/drivers/pca954x"
 
@@ -31,29 +41,29 @@ if [ $(fw_printenv fitconfig | grep everest) ]; then
     echo gpio-keys-polled > $gpiopath/unbind
 
     # bus 4
-    if [ -e $drvpath/4-0070 ]; then echo 4-0070 > $drvpath/unbind; fi
+    if [ -e $drvpath/4-0070 ]; then mux4=1; echo 4-0070 > $drvpath/unbind; fi
     reset_bus_4
-    echo 4-0070 > $drvpath/bind
+    if [ -n "$mux4" ]; then echo 4-0070 > $drvpath/bind; fi
 
     # buses 5, 6, and 14
-    if [ -e $drvpath/5-0070 ]; then echo 5-0070 > $drvpath/unbind; fi
-    if [ -e $drvpath/6-0070 ]; then echo 6-0070 > $drvpath/unbind; fi
-    if [ -e $drvpath/14-0070 ]; then echo 14-0070 > $drvpath/unbind; fi
-    if [ -e $drvpath/14-0071 ]; then echo 14-0071 > $drvpath/unbind; fi
+    if [ -e $drvpath/5-0070 ]; then mux5=1; echo 5-0070 > $drvpath/unbind; fi
+    if [ -e $drvpath/6-0070 ]; then mux6=1; echo 6-0070 > $drvpath/unbind; fi
+    if [ -e $drvpath/14-0070 ]; then mux140=1; echo 14-0070 > $drvpath/unbind; fi
+    if [ -e $drvpath/14-0071 ]; then mux141=1; echo 14-0071 > $drvpath/unbind; fi
     reset_buses_5_6_and_14
-    echo 5-0070 > $drvpath/bind
-    echo 6-0070 > $drvpath/bind
-    echo 14-0070 > $drvpath/bind
-    echo 14-0071 > $drvpath/bind
+    if [ -n "$mux5" ]; then echo 5-0070 > $drvpath/bind; fi
+    if [ -n "$mux6" ]; then echo 6-0070 > $drvpath/bind; fi
+    if [ -n "$mux140" ]; then echo 14-0070 > $drvpath/bind; fi
+    if [ -n "$mux141" ]; then echo 14-0071 > $drvpath/bind; fi
 
     # bus 15
-    if [ -e $drvpath/15-0070 ]; then echo 15-0070 > $drvpath/unbind; fi
-    if [ -e $drvpath/15-0071 ]; then echo 15-0071 > $drvpath/unbind; fi
-    if [ -e $drvpath/15-0072 ]; then echo 15-0072 > $drvpath/unbind; fi
+    if [ -e $drvpath/15-0070 ]; then mux150=1; echo 15-0070 > $drvpath/unbind; fi
+    if [ -e $drvpath/15-0071 ]; then mux151=1; echo 15-0071 > $drvpath/unbind; fi
+    if [ -e $drvpath/15-0072 ]; then mux152=1; echo 15-0072 > $drvpath/unbind; fi
     reset_bus_15
-    echo 15-0070 > $drvpath/bind
-    echo 15-0071 > $drvpath/bind
-    echo 15-0072 > $drvpath/bind
+    if [ -n "$mux150" ]; then  echo 15-0070 > $drvpath/bind; fi
+    if [ -n "$mux151" ]; then  echo 15-0071 > $drvpath/bind; fi
+    if [ -n "$mux152" ]; then  echo 15-0072 > $drvpath/bind; fi
 
     echo gpio-keys-polled > $gpiopath/bind
 fi
