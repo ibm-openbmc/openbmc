ibm-openbmc/openbmc

commit f3a9347e5844979082de5468b70bc9f548b4227e

libmctp: squash downstream changes
Change-Id: I6c65a918eec331443f8ad00e5e7f3509165c5580

meta-phosphor/recipes-phosphor/libmctp/libmctp_git.bb
@@ -12,6 +12,7 @@ LIC_FILES_CHKSUM = "file://LICENSE;md5=0d30807bb7a4f16d36e96b78f9ed8fae"
 SRC_URI = "git://github.com/openbmc/libmctp \
 	   file://default"
 SRCREV = "ae59f4fe3a3ceaf52a682ada2a2b54855fad2408"
+CONFFILES:${PN} = "${sysconfdir}/default/mctp"
 
 DEPENDS += "autoconf-archive-native \
             systemd \
@@ -29,9 +30,15 @@ PACKAGECONFIG[astlpc-raw-kcs] = "--enable-astlpc-raw-kcs,--disable-astlpc-raw-kc
 
 CONFFILES:${PN} = "${sysconfdir}/default/mctp"
 
+PACKAGECONFIG ??= "${@bb.utils.filter('DISTRO_FEATURES', 'systemd', d)}"
+PACKAGECONFIG[systemd] = "--with-systemdsystemunitdir=${systemd_system_unitdir}, \
+                          --without-systemdsystemunitdir,systemd"
+
+CONFFILES:${PN} = "${sysconfdir}/default/mctp"
+
 do_install:append() {
 	install -d ${D}${sysconfdir}/default
-	install -m 0644 ${WORKDIR}/default ${D}${sysconfdir}/default/mctp
+	install -m 0644 ${WORKDIR}/mctp ${D}${sysconfdir}/default/mctp
 }
 
 S = "${WORKDIR}/git"

commit b2840ed78522464e0f4e8d323d58147480e7f824

libmctp: correct failed merge
The removed lines are de-duplicated.  The duplication caused unexpected
behavior when the first PACKAGECONFIG ??= line was changed and
subsequently overwritten by the second instance of PACKAGECONFIG ??=

Fixes: f3a9347e5844979082de5468b70bc9f548b4227e
Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

meta-phosphor/recipes-phosphor/libmctp/libmctp_git.bb
@@ -30,12 +30,6 @@ PACKAGECONFIG[astlpc-raw-kcs] = "--enable-astlpc-raw-kcs,--disable-astlpc-raw-kc
 
 CONFFILES:${PN} = "${sysconfdir}/default/mctp"
 
-PACKAGECONFIG ??= "${@bb.utils.filter('DISTRO_FEATURES', 'systemd', d)}"
-PACKAGECONFIG[systemd] = "--with-systemdsystemunitdir=${systemd_system_unitdir}, \
-                          --without-systemdsystemunitdir,systemd"
-
-CONFFILES:${PN} = "${sysconfdir}/default/mctp"
-
 do_install:append() {
 	install -d ${D}${sysconfdir}/default
 	install -m 0644 ${WORKDIR}/mctp ${D}${sysconfdir}/default/mctp