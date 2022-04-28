c637bdb30d560754fbf25ebfcbd3d9c61835bd1a
Jayanth Othayoth <ojayanth@in.ibm.com>
2022-04-27 23:23:31 -0500

    openpower: pdata DEVTREE path updates
    Updated phal DEVTREE partition to hostfw path
    
    Signed-off-by: Jayanth Othayoth <ojayanth@in.ibm.com>
    Change-Id: Ie07c885cd4bc5cc0f824d20317cc72572b618733

--- meta-openpower/recipes-bsp/pdata/files/power-target.sh
+++ meta-openpower/recipes-bsp/pdata/files/power-target.sh
@@ -2,6 +2,6 @@
 # PDBG_DTB environment variable.
 # attributes tool required PDBG_DTB, PDATA_INFODB and PDATA_ATTR_OVERRIDE
 
-export PDBG_DTB=/var/lib/phosphor-software-manager/pnor/rw/DEVTREE
+export PDBG_DTB=/var/lib/phosphor-software-manager/hostfw/running/DEVTREE
 export PDATA_INFODB=/usr/share/pdata/attributes_info.db
 export PDATA_ATTR_OVERRIDE=/tmp/devtree_attr_override
