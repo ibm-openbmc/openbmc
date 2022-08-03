b8cc0b03a3669d65a26e31a5b3087cf6e3e49596
Andrew Geissler <geissonator@users.noreply.github.com>
2022-08-03 13:13:15 -0500

    meta-ibm: add the change-logo service for IPS and OEM (#233)
    * meta-ibm: Add the change-logo service for IPS
    
    Add change-logo service to detect whether there is a keyword in the
    VPD, if it exists, the original logo will be replaced with a inspur
    logo.
    
    issues: https://github.com/ibm-openbmc/dev/issues/2895
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    
    * meta-ibm: change-logo fixes
    
    Some fixes found in testing this PR
    
    Tested:
    - Verified default path when brand is IBM
    - Verified IPS logo loaded in GUI when brand is J0
    - Verified transparent logo loaded for any other brand
    
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>
    
    Co-authored-by: George Liu <liuxiwei@inspur.com>

---
+++ meta-ibm/recipes-phosphor/webui/webui-vue/changeLogo.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+
+model=($(busctl get-property xyz.openbmc_project.Inventory.Manager /xyz/openbmc_project/inventory/system xyz.openbmc_project.Inventory.Decorator.Asset SubModel))
+DIR=/usr/share/www/img
+if [ "${model[1]}" = '"S0"' ]  || [ "${model[1]}" = '"D0"' ]; then
+    echo "Using IBM logo"
+elif [ "${model[1]}" = '"J0"' ]; then
+    echo "loading IPS logo"
+    mount --bind ${DIR}/inspur-login-logo.svg.gz ${DIR}/login-company-logo.svg.gz
+    mount --bind ${DIR}/inspur-logo-header.svg.gz ${DIR}/logo-header.svg.gz
+    mount --bind ${DIR}/blankLogo.svg.gz  /usr/share/www/bee-2-light.svg.gz
+else
+    echo "loading OEM logo"
+    mount --bind ${DIR}/blankLogo.svg.gz ${DIR}/login-company-logo.svg.gz
+    mount --bind ${DIR}/blankLogo.svg.gz ${DIR}/logo-header.svg.gz
+    mount --bind ${DIR}/blankLogo.svg.gz  /usr/share/www/bee-2-light.svg.gz
+fi
