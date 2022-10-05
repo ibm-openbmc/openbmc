896bdf66e1d499f57552950f450bd1637b538d6d
George Liu <liuxiwei@inspur.com>
2022-10-05 16:54:53 +0800

    Support css style for IPS
    Replacing the css style of the IPS according to the J0 of the VPD.
    
    Also, Update the IPS logo.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>

--- meta-ibm/recipes-phosphor/webui/webui-vue/changeLogo.sh
+++ meta-ibm/recipes-phosphor/webui/webui-vue/changeLogo.sh
@@ -2,6 +2,7 @@
 
 model=($(busctl get-property xyz.openbmc_project.Inventory.Manager /xyz/openbmc_project/inventory/system xyz.openbmc_project.Inventory.Decorator.Asset SubModel))
 DIR=/usr/share/www/img
+CSS_DIR=/usr/share/www/css
 if [ "${model[1]}" = '"S0"' ]  || [ "${model[1]}" = '"D0"' ]; then
     echo "Using IBM logo"
 elif [ "${model[1]}" = '"J0"' ]; then
@@ -9,6 +10,7 @@ elif [ "${model[1]}" = '"J0"' ]; then
     mount --bind ${DIR}/inspur-login-logo.svg.gz ${DIR}/login-company-logo.svg.gz
     mount --bind ${DIR}/inspur-logo-header.svg.gz ${DIR}/logo-header.svg.gz
     mount --bind ${DIR}/blankLogo.svg.gz  /usr/share/www/bee-2-light.svg.gz
+    mount --bind ${CSS_DIR}/ips.app.css.gz  ${CSS_DIR}/app.css.gz
 else
     echo "loading OEM logo"
     mount --bind ${DIR}/blankLogo.svg.gz ${DIR}/login-company-logo.svg.gz
